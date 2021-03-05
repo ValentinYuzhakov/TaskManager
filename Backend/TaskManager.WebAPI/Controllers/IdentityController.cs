1064a585ca10b55e2b76e37a1d944a7fc9773384
UzhakovValentin <yuz-valya@yandex.ru>
2021-03-05 21:05:40 +0300

    add identity controller, delete registration and auth controllers

--- Backend/TaskManager.WebAPI/Controllers/IdentityController.cs
+++ Backend/TaskManager.WebAPI/Controllers/IdentityController.cs
@@ -6,16 +6,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TaskManager.Core.Auth.Interfaces;
+using TaskManager.Core.Services.Interfaces;
 using TaskManager.Domain.Models;
+using TaskManager.Shared.ViewModels;
+using TaskManager.Shared;
+using TaskManager.Shared.Infos;
 
 namespace TaskManager.WebAPI.Controllers
 {
-    [Route("api/Identity")]
     [ApiController]
+    [Route("api/Identity")]
     public class IdentityController : SystemController
     {
-        public IdentityController(UserManager<User> userManager) : base(userManager)
+        public IdentityController(UserManager<User> userManager) : base(userManager) { }
+
+
+        [HttpPost("registration")]
+        public async Task<Guid> Register([FromBody] UserRegistrationInfo request,
+            [FromServices] IUserService userService)
         {
+            return await userService.Create(request);
+        }
+
+        [HttpPost("authorization")]
+        public async Task<UserAuthorizeView> Authorize([FromBody] UserAuthorizeInfo request,
+            [FromServices] IUserService userService)
+        {
+            return await userService.Authorize(request);
         }
 
         [HttpPut("confirm-email/{token}")]
